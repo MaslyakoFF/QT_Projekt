4722bac977a3c3288eece88f85a16b5d633bd91a
Alexander <masal2002@mail.ru>
2019-01-31 22:53:00 +0300

    все плохо

--- kubik_rubika.py
+++ kubik_rubika.py
@@ -2,14 +2,15 @@ import random
 import sys
 from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QFrame
+from PyQt5.QtGui import QPainter, QColor
 
 
 class Cube:
     def __init__(self):
         self.front = []
-        self.left = []
+        self.left1 = []
         self.back = []
-        self.right = []
+        self.right1 = []
         self.up = []
         self.down = []
 
@@ -29,7 +30,7 @@ class Cube:
             del colors[x]
             ind -= 1
             x = random.randint(-1, ind)
-            self.left.append(colors[x])
+            self.left1.append(colors[x])
             del colors[x]
             ind -= 1
             x = random.randint(-1, ind)
@@ -37,7 +38,7 @@ class Cube:
             del colors[x]
             ind -= 1
             x = random.randint(-1, ind)
-            self.right.append(colors[x])
+            self.right1.append(colors[x])
             del colors[x]
             ind -= 1
             x = random.randint(-1, ind)
@@ -49,9 +50,9 @@ class Cube:
             del colors[x]
             ind -= 1
         self.front.append('red')
-        self.left.append('green')
+        self.left1.append('green')
         self.back.append('orange')
-        self.right.append('blue')
+        self.right1.append('blue')
         self.up.append('black')
         self.down.append('yellow')
         for i in range(4):
@@ -60,7 +61,7 @@ class Cube:
             del colors[x]
             ind -= 1
             x = random.randint(-1, ind)
-            self.left.append(colors[x])
+            self.left1.append(colors[x])
             del colors[x]
             ind -= 1
             x = random.randint(-1, ind)
@@ -68,7 +69,7 @@ class Cube:
             del colors[x]
             ind -= 1
             x = random.randint(-1, ind)
-            self.right.append(colors[x])
+            self.right1.append(colors[x])
             del colors[x]
             ind -= 1
             x = random.randint(-1, ind)
@@ -85,6 +86,9 @@ class Operations(QMainWindow, QFrame, Cube):
     def __init__(self):
         super().__init__()
         uic.loadUi('window.ui', self)
+        self.start()
+
+    def initUI(self):
         self.l_u_u.clicked.connect(self.spin_l_u_u)
         self.c_u_u.clicked.connect(self.spin_c_u_u)
         self.r_u_u.clicked.connect(self.spin_r_u_u)
@@ -102,15 +106,11 @@ class Operations(QMainWindow, QFrame, Cube):
         self.spinRight.clicked.connect(self.spin_right)
         self.spinUp.clicked.connect(self.spin_up)
         self.restart.clicked.connect(self.start)
-        self.front = []
-        self.left = []
-        self.back = []
-        self.right = []
-        self.up = []
-        self.down = []
+
+        self.show()
 
     def spin_right(self):
-        self.front[:], self.left[:], self.back[:], self.right[:] = self.left[:], self.back[:], self.right[:], \
+        self.front[:], self.left1[:], self.back[:], self.right1[:] = self.left1[:], self.back[:], self.right1[:], \
                                                                    self.front[:]
         self.up[0], self.up[1], self.up[2], self.up[5], self.up[8], self.up[7], self.up[6], \
         self.up[3] = self.up[2], self.up[5], self.up[8], self.up[7], self.up[6], self.up[3], \
@@ -118,10 +118,10 @@ class Operations(QMainWindow, QFrame, Cube):
         self.down[0], self.down[1], self.down[2], self.down[5], self.down[8], self.down[7], self.down[6], \
         self.down[3] = self.down[2], self.down[5], self.down[8], self.down[7], self.down[6], self.down[3], \
                        self.down[0], self.down[1]
-        self.print_edge
+        self.paintEvent
 
     def spin_left(self):
-        self.front[:], self.right[:], self.back[:], self.left[:] = self.left[:], self.front[:], self.right[:], \
+        self.front[:], self.right1[:], self.back[:], self.left1[:] = self.left1[:], self.front[:], self.right1[:], \
                                                                    self.back[:]
         self.up[0], self.up[1], self.up[2], self.up[5], self.up[8], self.up[7], self.up[6], self.up[3] = self.up[6], \
                                                                                                          self.up[3], \
@@ -139,23 +139,23 @@ class Operations(QMainWindow, QFrame, Cube):
     def spin_up(self):
         self.front[:], self.up[:], self.back[:], self.down[:] = self.down[6:] + self.down[3:6] + self.down[:3], \
                                                                 self.front[:], self.up[::-1], self.back[::-1]
-        self.right[0], self.right[1], self.right[2], self.right[5], self.right[8], self.right[7], self.right[6], \
-        self.right[3] = self.right[6], self.right[3], self.right[0], self.right[1], self.right[2], self.right[5], \
-                       self.right[8], self.right[7]
-        self.left[0], self.left[1], self.left[2], self.left[5], self.left[8], self.left[7], self.left[6], \
-        self.left[3] = self.left[2], self.left[5], self.left[8], self.left[7], self.left[6], self.left[3], \
-                     self.left[0], self.left[1]
+        self.right1[0], self.right1[1], self.right1[2], self.right1[5], self.right1[8], self.right1[7], self.right1[6], \
+        self.right1[3] = self.right1[6], self.right1[3], self.right1[0], self.right1[1], self.right1[2], self.right1[5], \
+                       self.right1[8], self.right1[7]
+        self.left1[0], self.left1[1], self.left1[2], self.left1[5], self.left1[8], self.left1[7], self.left1[6], \
+        self.left1[3] = self.left1[2], self.left1[5], self.left1[8], self.left1[7], self.left1[6], self.left1[3], \
+                     self.left1[0], self.left1[1]
         self.print_edge
 
     def spin_down(self):
         self.front [:], self.up[:], self.back[:], self.down[:] = self.up[:], self.back[::-1], self.down[::-1], \
                                                                  self.front[6:] + self.front[3:6] + self.front[:3]
-        self.left[0], self.left[1], self.left[2], self.left[5], self.left[8], self.left[7], self.left[6], \
-        self.left[3] = self.left[6], self.left[3], self.left[0], self.left[1], self.left[2], self.left[5], \
-                        self.left[8], self.left[7]
-        self.right[0], self.right[1], self.right[2], self.right[5], self.right[8], self.right[7], self.right[6], \
-        self.right[3] = self.right[2], self.right[5], self.right[8], self.right[7], self.right[6], self.right[3], \
-                       self.right[0], self.right[1]
+        self.left1[0], self.left1[1], self.left1[2], self.left1[5], self.left1[8], self.left1[7], self.left1[6], \
+        self.left1[3] = self.left1[6], self.left1[3], self.left1[0], self.left1[1], self.left1[2], self.left1[5], \
+                        self.left1[8], self.left1[7]
+        self.right1[0], self.right1[1], self.right1[2], self.right1[5], self.right1[8], self.right1[7], self.right1[6], \
+        self.right1[3] = self.right1[2], self.right1[5], self.right1[8], self.right1[7], self.right1[6], self.right1[3], \
+                       self.right1[0], self.right1[1]
         self.print_edge
 
     def spin_c_u_u(self):
@@ -167,8 +167,8 @@ class Operations(QMainWindow, QFrame, Cube):
         self.print_edge
 
     def spin_c_r_r(self):
-        self.front[3:6], self.right[3:6], self.back[3:6], self.left[3:6] = self.left[3:6],  self.front[3:6], \
-                                                                           self.right[3:6], self.back[3:6]
+        self.front[3:6], self.right1[3:6], self.back[3:6], self.left1[3:6] = self.left1[3:6],  self.front[3:6], \
+                                                                           self.right1[3:6], self.back[3:6]
         self.print_edge
 
     def spin_c_d_d(self):
@@ -180,12 +180,12 @@ class Operations(QMainWindow, QFrame, Cube):
         self.print_edge
 
     def spin_c_l_l(self):
-        self.front[3:6], self.right[3:6], self.back[3:6], self.left[3:6] = self.right[3:6], self.back[3:6], \
-                                                                           self.left[3:6], self.front[3:6]
+        self.front[3:6], self.right1[3:6], self.back[3:6], self.left1[3:6] = self.right1[3:6], self.back[3:6], \
+                                                                           self.left1[3:6], self.front[3:6]
         self.print_edge
 
     def spin_l_u_l(self):
-        self.front[:4], self.right[:4], self.back[:4], self.left[:4] = self.right[:4], self.back[:4], self.left[:4], \
+        self.front[:4], self.right1[:4], self.back[:4], self.left1[:4] = self.right1[:4], self.back[:4], self.left1[:4], \
                                                                        self.front[:4]
         self.up[0], self.up[1], self.up[2], self.up[5], self.up[8], self.up[7], self.up[6], self.up[3] = self.up[6], \
                                                                                                          self.up[3], \
@@ -198,7 +198,7 @@ class Operations(QMainWindow, QFrame, Cube):
         self.print_edge
 
     def spin_l_d_l(self):
-        self.front[6:], self.right[6:], self.back[6:], self.left[6:] = self.right[6:], self.back[6:], self.left[6:], \
+        self.front[6:], self.right1[6:], self.back[6:], self.left1[6:] = self.right1[6:], self.back[6:], self.left1[6:], \
                                                                       self.front[6:]
         self.down[0], self.down[1], self.down[2], self.down[5], self.down[8], self.down[7], self.down[6], \
         self.down[5] = self.down[6], self.down[3], self.down[0], self.down[1], self.down[2], self.down[5], \
@@ -206,7 +206,7 @@ class Operations(QMainWindow, QFrame, Cube):
         self.print_edge
 
     def spin_r_u_r(self):
-        self.front[:4], self.left[:4], self.back[:4], self.right[:4] = self.left[:4], self.back[:4], self.right[:4], \
+        self.front[:4], self.left1[:4], self.back[:4], self.right1[:4] = self.left1[:4], self.back[:4], self.right1[:4], \
                                                                        self.front[:4]
         self.up[0], self.up[1], self.up[2], self.up[5], self.up[8], self.up[7], self.up[6], \
         self.up[3] = self.up[2], self.up[5], self.up[8], self.up[7], self.up[6], self.up[3], \
@@ -214,7 +214,7 @@ class Operations(QMainWindow, QFrame, Cube):
         self.print_edge
 
     def spin_r_d_r(self):
-        self.front[6:], self.left[6:], self.back[6:], self.right[6:] = self.left[6:], self.back[6:], self.right[6:], \
+        self.front[6:], self.left1[6:], self.back[6:], self.right1[6:] = self.left1[6:], self.back[6:], self.right1[6:], \
                                                                        self.front[6:]
         self.down[0], self.down[1], self.down[2], self.down[5], self.down[8], self.down[7], self.down[6], \
         self.down[3] = self.down[2], self.down[5], self.down[8], self.down[7], self.down[6], self.down[3], \
@@ -227,9 +227,9 @@ class Operations(QMainWindow, QFrame, Cube):
                                                                  self.front[0], self.front[3], self.fron[6], \
                                                                  self.up[0], self.up[3], self.up[6], self.back[8], \
                                                                  self.back[5], self.back[2]
-        self.left[0], self.left[1], self.left[2], self.left[5], self.left[8], self.left[7], self.left[6], \
-        self.left[3] = self.left[2], self.left[5], self.left[8], self.left[7], self.left[6], self.left[3], \
-                       self.left[0], self.left[1]
+        self.left1[0], self.left1[1], self.left1[2], self.left1[5], self.left1[8], self.left1[7], self.left1[6], \
+        self.left1[3] = self.left1[2], self.left1[5], self.left1[8], self.left1[7], self.left1[6], self.left1[3], \
+                       self.left1[0], self.left1[1]
         self.print_edge
 
     def spin_l_d_d(self):
@@ -238,9 +238,9 @@ class Operations(QMainWindow, QFrame, Cube):
                                                                  self.back[5], self.back[2], self.down[6], \
                                                                  self.down[3], self.down[0], self.front[0], \
                                                                  self.front[3], self.fron[6]
-        self.left[0], self.left[1], self.left[2], self.left[5], self.left[8], self.left[7], self.left[6], \
-        self.left[3] = self.left[6], self.left[3], self.left[0], self.left[1], self.left[2], self.left[5], \
-                       self.left[8], self.left[7]
+        self.left1[0], self.left1[1], self.left1[2], self.left1[5], self.left1[8], self.left1[7], self.left1[6], \
+        self.left1[3] = self.left1[6], self.left1[3], self.left1[0], self.left1[1], self.left1[2], self.left1[5], \
+                       self.left1[8], self.left1[7]
         self.print_edge
 
     def spin_r_u_u(self):
@@ -249,9 +249,9 @@ class Operations(QMainWindow, QFrame, Cube):
                                                                  self.front[2], self.front[5], self.front[8], \
                                                                  self.up[2], self.up[5], self.up[8], self.back[6], \
                                                                  self.back[3], self.back[0]
-        self.right[0], self.right[1], self.right[2], self.right[5], self.right[8], self.right[7], self.right[6], \
-        self.right[3] = self.right[6], self.right[3], self.right[0], self.right[1], self.right[2], self.right[5], \
-                        self.right[8], self.right[7]
+        self.right1[0], self.right1[1], self.right1[2], self.right1[5], self.right1[8], self.right1[7], self.right1[6], \
+        self.right1[3] = self.right1[6], self.right1[3], self.right1[0], self.right1[1], self.right1[2], self.right1[5], \
+                        self.right1[8], self.right1[7]
         self.print_edge
 
     def spin_r_d_d(self):
@@ -260,25 +260,39 @@ class Operations(QMainWindow, QFrame, Cube):
                                                                  self.back[3], self.back[0], self.down[8], \
                                                                  self.down[5], self.down[2], self.front[2], \
                                                                  self.front[5], self.front[8]
-        self.right[0], self.right[1], self.right[2], self.right[5], self.right[8], self.right[7], self.right[6], \
-        self.right[3] = self.right[2], self.right[5], self.right[8], self.right[7], self.right[6], self.right[3], \
-                        self.right[0], self.right[1]
+        self.right1[0], self.right1[1], self.right1[2], self.right1[5], self.right1[8], self.right1[7], self.right1[6], \
+        self.right1[3] = self.right1[2], self.right1[5], self.right1[8], self.right1[7], self.right1[6], self.right1[3], \
+                        self.right1[0], self.right1[1]
         self.print_edge
 
-    def print_edge(self):
-        self.l_left_up.setText(self.front[0])
-        self.l_center_up.setText(self.front[1])
-        self.l_right_up.setText(self.front[2])
-        self.l_leftcenter.setText(self.front[3])
-        self.l_center.setText(self.front[4])
-        self.l_right_centersetText(self.front[5])
-        self.l_left_down.setText(self.front[6])
-        self.l_center_down.setText(self.front[7])
-        self.l_right_down.setText(self.front[8])
+    def paintEvent(self, event):
+        qp = QPainter()
+        qp.begin(self)
+        self.print_edge(qp)
+        qp.end()
+
+    def print_edge(self, qp):
+        qp.setBrush(QColor(self.front[0]))
+        qp.drawRect(50, 50, 50, 50)
+        qp.setBrush(QColor(self.front[1]))
+        qp.drawRect(100, 50, 50, 50)
+        qp.setBrush(QColor(self.front[2]))
+        qp.drawRect(150, 50, 50, 50)
+        qp.setBrush(QColor(self.front[3]))
+        qp.drawRect(50, 100, 50, 50)
+        qp.setBrush(QColor(self.front[4]))
+        qp.drawRect(100, 100, 50, 50)
+        qp.setBrush(QColor(self.front[5]))
+        qp.drawRect(150, 100, 50, 50)
+        qp.setBrush(QColor(self.front[6]))
+        qp.drawRect(50, 150, 50, 50)
+        qp.setBrush(QColor(self.front[7]))
+        qp.drawRect(100, 150, 50, 50)
+        qp.setBrush(QColor(self.front[8]))
+        qp.drawRect(150, 150, 50, 50)
 
 
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    ex = Cube()
-    ex.show()
-    sys.exit(app.exec())
+app = QApplication(sys.argv)
+ex = Operations()
+ex.show()
+sys.exit(app.exec_())
